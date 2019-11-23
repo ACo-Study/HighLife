773d469625c1af831d07d2b7412959995a2d3f0c
Main-Brain <rlswnsehdehd@naver.com>
2019-11-23 21:12:02 +0900

    [MainBrain] 로그인, 회원가입 폼 추가

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,80 +1,79 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 class Authentication extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            password: ""
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleChange(event) {
-        this.setState({ value: event.target.value });
-    }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
 
-    handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
-    }
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.state.value);
+    event.preventDefault();
+  }
 
-    render() {
-        const inputBoxes = (
-            <div>
-                <label>Username</label>
-                <input
-                    name="username"
-                    type="text"
-                    className="validate"
-                    onChange={this.handleChange}
-                    value={this.state.username}
-                />
-                <label>Password</label>
-                <input
-                    name="password"
-                    type="password"
-                    className="validate"
-                    onChange={this.handleChange}
-                    value={this.state.password}
-                />
-            </div>
-        );
+  render() {
+    const inputBoxes = (
+      <div>
+        <input
+          name="username"
+          type="text"
+          placeholder="아이디(이메일)"
+          onChange={this.handleChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="비밀번호"
+          onChange={this.handleChange}
+        />
+      </div>
+    );
 
-        const loginView = (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input type="submit" value="Submit" />
-                    <Link to="/register">회원가입</Link>
-                </form>
-            </div>
-        );
+    const loginView = (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          {inputBoxes}
+          <input type="submit" value="로그인" />
+          <Link to="/register">회원가입</Link>
+        </form>
+      </div>
+    );
 
-        const registerView = (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
-        );
+    const registerView = (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          {inputBoxes}
+          <input type="submit" value="회원가입" />
+        </form>
+      </div>
+    );
 
-        return (
-            <div>
-                <h1>{this.props.isMember ? "로그인" : "회원가입"}</h1>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <h1>{this.props.isMember ? loginView : registerView}</h1>
+      </div>
+    );
+  }
 }
 
 Authentication.propTypes = {
-    isMember: PropTypes.bool
+  isMember: PropTypes.bool
 };
 
 Authentication.defaultProps = {
-    isMember: true
+  isMember: true
 };
 
 export default Authentication;
